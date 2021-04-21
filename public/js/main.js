c2edf36e6072eb5195d18e96597a1db6a54fa84a
AndyLinxies <linxies@gmail.com>
2021-04-21 14:10:42 +0200

    Exo1 For

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+//Exo1
+// let eleves = ["Alexis", "Andy", "Charles", "Dorian", "Farhad", "Ilyas", "Junior", "Kev", "Loïc", "Mihai", "Oussama", "Tania", "William", "Alain"];
+
+// for (let i = 0; i < eleves.length; i++){
+//     console.log('Bonjour '+eleves[i]);
+// }
+
+//Exo2
+let nombre = +prompt('Inserez un nombre')
+
+for (i=0; i <= nombre ; i++){
+    console.log(i);
+}
